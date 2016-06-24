b6e2a60feb3772f4839570a6e8dfb98cd7c7cf41
roshan556 <roshan50233@gmail.com>
2016-06-24 07:37:23 +0545

    nitial project

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,69 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFA.Fourm.ConoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User Name:");
+            string userName = Console.ReadLine();
+            Console.Write("Password:");
+            string password = string.Empty;
+
+            password = ReadPassword(password);
+            bool isValid = Authenticate(userName, password);
+
+            if (isValid)
+            {
+                //For Admin User 
+                Console.WriteLine("---Menu---");
+                Console.WriteLine("---Manage User---");
+                Console.WriteLine("---Change Password---");
+            }
+
+            //Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+
+        static bool Authenticate(string userName, string password)
+        {
+            bool isValid = true;
+
+            if ((userName == "admin") && (password == "password"))
+            {
+                Console.WriteLine("Welcome " + userName);
+            }
+            else if ((userName == "user") && (password == "password"))
+            {
+                Console.WriteLine("welcome " + userName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Login");
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        private static string ReadPassword(string password)
+        {
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                info = Console.ReadKey(true);
+            }
+            return password;
+        }
+    }
+}
